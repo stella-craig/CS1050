44e6eb92537070f2a10e123c25c7f81fe7f5ac74
Stella <112723364+stella-craig@users.noreply.github.com>
2022-09-20 15:27:31 -0500

    Update lab1.c

--- lab1.c
+++ lab1.c
@@ -1,6 +1,11 @@
-#include<stdio.h>
+/* This is the very first program I have written.
+   When the program runs, it will return "Hello World"
+   to the user. A very basic program, but everyone has
+   to start somewhere. :) */
 
-int main(void)
+#include<stdio.h> //Will "include" a pre-built library of different functions that we can use
+
+int main(void) //The "driver" portion of our code: when we compile the code it will know to look for a function called "main" in order to know where to start the program
 {
-        printf("Hello World!\n");
+        printf("Hello World!\n"); //Will print whatever is in the quotation marks
 }
